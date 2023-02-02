f7f6c2d724da5441826f5a75c3420e8ac5419b85
danielOuattara <daniel.ouattara@gmx.com>
2023-02-02 14:11:00 +0100

    starting glog app again gatsby 5. on md plugins

---
+++ blog_app_2023_gatsby5/src/components/layout.js
@@ -0,0 +1,55 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
+
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+import Header from "./header";
+import Footer from "./footer";
+import "./layout.css";
+import Menu from "./menu";
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <main
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+      >
+        <Menu />
+        {children}
+      </main>
+      <Footer
+        style={{
+          margin: `0 auto`,
+          maxWidth: `var(--size-content)`,
+          padding: `var(--size-gutter)`,
+        }}
+        siteDescription={data.site.siteMetadata?.description}
+        siteAuthor={data.site.siteMetadata?.author}
+      />
+    </>
+  );
+};
+
+export default Layout;
